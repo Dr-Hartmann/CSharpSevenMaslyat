f3b2f986d49aae054cbec630ef792ea04dd42f85
Saifor <saifor@bk.ru>
2025-03-21 08:28:09 +0300

    Start and creation of the project
    Start and creation of the project (Will be updated soon)

---
+++ TestSection/Database/Program/Program/Context.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Program
+{
+    class Context : DbContext
+    {
+        public Context()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(@"host=localhost;port=5532;database=db;username=root;pass=5532");
+        }
+
+        public DbSet<Book> Book {get; set; }
+    }
+
+    //public class BookContext : DbContext
+    //{
+    //    public DbSet<Book> Books { get; set; }
+    //    public DbSet<Page> Pages { get; set; }
+
+    //    public BookContext(DbContextOptions<BookContext> options) : base(options)
+    //    {
+    //    }
+
+    //    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    //    {
+    //        // Явное указание связи (необязательно, но рекомендуется для контроля)
+    //        modelBuilder.Entity<Book>()
+    //            .HasMany(b => b.Pages)
+    //            .WithOne(p => p.Book)
+    //            .HasForeignKey(p => p.BookId); // BookId – внешний ключ в таблице Pages
+
+    //        base.OnModelCreating(modelBuilder);
+    //    }
+    //}
+}
