8bc905eb7f7a5c32df21ce00ea1cb9ee16795c6c
Saifor <58148272+Saifor@users.noreply.github.com>
2025-03-21 23:21:57 +0300

    Database (#2)
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * MyFirstTestCommit
    
    Test commit hi
    
    * Revert "MyFirstTestCommit"
    
    This reverts commit 8a5b2f1ef6e10cecfe3cb8091e5ba65efd492599.
    
    * Start and creation of the project
    
    Start and creation of the project (Will be updated soon)
    
    * Deleting unnecessary folders in my opinion
    
    GL HF
    EZ GG WP
    
    * DB Connected
    
    * Deleted more folders
    
    * 已移除 db.cs (No longer needed)
    
    已移除 db.cs (不再需要)
    
    * -NoChanges-
    
    Sorry, but we need to do this...
    
    ---------
    
    Co-authored-by: Forguebeelov <134870596+Forguebeelov@users.noreply.github.com>

---
+++ TestSection/Database/Program/Program/Context.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Program.Entities;
+
+namespace Program
+{
+    class Context : DbContext
+    {
+        public Context()
+        {
+            Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(@"host=localhost;port=5532;database=7maslyat;username=postgres;password=553210zxc");
+        }
+
+        public DbSet<Book> Book {get; set; }
+        public DbSet<Page> Page { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Явное указание связи (необязательно, но рекомендуется для контроля)
+            modelBuilder.Entity<Book>()
+                .HasMany(b => b.Pages)
+                .WithOne(p => p.Book)
+                .HasForeignKey(p => p.BookId); // BookId – внешний ключ в таблице Pages
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+        
+}
---
+++ TestSection/Database/Program/Program/Entities/Book.cs
@@ -0,0 +1,17 @@
+﻿namespace Program.Entities
+{
+    class Book
+    {
+        public int Id { get; set; } //Id
+        public string Title { get; set; } //Заголовок
+        public string Font { get; set; } //Тип шрифта
+        public int Size { get; set; } //Размер шрифта
+        public string Align { get; set; } //Выравнивание
+        public double Interv { get; set; } //Межстрочный интервал
+        public double Indent { get; set; } //Отступ
+        public string Outline { get; set; } //Начертание (Жирный/курсив)
+        public bool Bef_parag { get; set; } //Наличие отступа перед абзацем
+        public bool Aft_parag { get; set; } //Наличие отступа после абзаца
+        public List<Page> Pages { get; set; } = new List<Page>(); //Количество страниц
+    }
+}
