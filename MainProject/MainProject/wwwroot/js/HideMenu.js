c1fbbfe1bae265bebbfe7ca44ba51a7837cb5708
GONEVladd20 <vladislavtikhomirov35@gmail.com>
2025-04-17 18:02:38 +0300

    feat: added pop-up menu

---
+++ MainProject/MainProject/wwwroot/js/HideMenu.js
@@ -0,0 +1,13 @@
+﻿window.registerOutsideClick = function (elementId, dotNetHelper) {
+    function onClick(event) {
+        const el = document.getElementById(elementId);
+        if (el && !el.contains(event.target)) {
+            dotNetHelper.invokeMethodAsync('HidePopup');
+            document.removeEventListener('click', onClick);
+        }
+    }
+
+    setTimeout(() => {
+        document.addEventListener('click', onClick);
+    }, 0);
+}
