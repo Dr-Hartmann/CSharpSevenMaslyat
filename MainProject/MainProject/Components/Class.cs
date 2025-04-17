bf09bbabd6e1facc8a42adfff7de790f3f75a6c8
GONEVladd20 <vladislavtikhomirov35@gmail.com>
2025-04-17 18:02:38 +0300

    feat: added pop-up menu

---
+++ MainProject/MainProject/Components/Class.cs
@@ -0,0 +1,12 @@
+﻿namespace DropList
+{
+    public class DropList
+    {
+        public bool ShowDiv { get; set; } = true;
+        public bool click()
+        {
+            ShowDiv =! ShowDiv;
+            return ShowDiv;
+        }
+    }
+}
