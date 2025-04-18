ba54e0ea594b9aab490c7c8c1e69ff57330a8b69
Stepan <164225344+Seelane@users.noreply.github.com>
2025-04-18 11:28:23 +0300

    added comments to many files
    По совету Тимлида, был взят и изучен его проект, а также добавлены комментарии, чтобы было проще ориентироваться в коде

--- MainProject/AspNetCoreMVC/AspNetCoreMVC/Data/AspNetCoreMVCContext.cs
+++ MainProject/AspNetCoreMVC/AspNetCoreMVC/Data/AspNetCoreMVCContext.cs
@@ -7,13 +7,29 @@ using AspNetCoreMVC.Models;
 
 namespace AspNetCoreMVC.Data
 {
+    /// <summary>
+    /// Контекст базы данных для приложения.
+    /// Наследуется от DbContext и предоставляет доступ к таблицам базы данных.
+    /// </summary>
     public class AspNetCoreMVCContext : DbContext
     {
+        /// <summary>
+        /// Конструктор контекста базы данных.
+        /// </summary>
+        /// <param name="options">
+        /// Опции для настройки контекста базы данных.
+        /// Включает строку подключения, провайдер базы данных и другие настройки.
+        /// </param>
         public AspNetCoreMVCContext (DbContextOptions<AspNetCoreMVCContext> options)
             : base(options)
         {
         }
 
+        /// <summary>
+        /// Свойство для доступа к таблице Movies в базе данных.
+        /// DbSet<T> представляет собой коллекцию сущностей определенного типа.
+        /// Используется для выполнения операций CRUD с таблицей Movies.
+        /// </summary>
         public DbSet<AspNetCoreMVC.Models.Movies> Movies { get; set; } = default!;
     }
 }
